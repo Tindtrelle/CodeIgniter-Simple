9fed8aa6a93996aa94d3058e05664251b88e257e
Tindtrelle <maloparac@hotmail.com>
2013-12-05 21:00:05 +0100

    DB info

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'licnoisk_admin';
-$db['default']['password'] = 'aprila02';
-$db['default']['database'] = 'licnoisk_ankajcan';
+$db['default']['hostname'] = '';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
